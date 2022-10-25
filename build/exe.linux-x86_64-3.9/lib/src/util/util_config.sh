1154ec9f9af75c7b80167e5ab81a40f777b314c9
enio ricardo de farias <eniodefarias@gmail.com>
2022-10-25 05:54:27 -0300

    Requeriments

--- build/exe.linux-x86_64-3.9/lib/src/util/util_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#!/usr/bin/awk
-
-config='/var/robo/ROBOS2/Adapter/config/config.ini'
-section=$1
-param=$2
-sed -nr "/^\[$section\]/ { :l /^$param[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $config
